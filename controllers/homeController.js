4aa4e33a78d7c55849cdb53345be701d9b4598f2
rioxraman <ramanpkgupta@gmail.com>
2023-03-02 17:23:00 +0530

    firt contoller

---
+++ controllers/homeController.js
@@ -0,0 +1,5 @@
+const home = function (req,res) {
+    return res.end("<h1> express is fired up </h1>")
+}
+
+module.exports = {home}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-
+const homeController=  require('../controllers/homeController')
 const router = express.Router();
 
 console.log("router is workrig");
+
+router.get('/',homeController.home)
 
 module.exports = router 
