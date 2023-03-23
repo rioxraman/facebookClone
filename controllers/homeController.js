6b75d34dc067da02d3aa3a4e1f049be17bf3f088
rioxraman <ramanpkgupta@gmail.com>
2023-03-23 16:25:56 +0530

    comments views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,15 @@
 const Post = require("../models/post")
 
 module.exports.home =  (req,res)=> {
-    Post.find({}).populate('user').exec(function (err,posts) {
+    Post.find({}).populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+
+    .exec(function (err,posts) {
         if(err){
             console.log(err);
             return
