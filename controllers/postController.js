cd398d195ce34b0e09e9220a7ec741c04d42a506
rioxraman <ramanpkgupta@gmail.com>
2023-03-24 16:24:42 +0530

    post destroy controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,4 @@
+const Comment = require("../models/comment");
 const Post = require("../models/post")
 
 module.exports.create = function(req,res){
@@ -11,4 +12,18 @@ module.exports.create = function(req,res){
         }
         return res.redirect('back')
     })
+}
+
+module.exports.destroy = function(req,res){
+   Post.findById(req.params.id,function(err,post){
+    if(post.user == req.user.id){
+        post.remove();
+        Comment.deleteMany({post:req.params.id},function(err){
+            console.log(err);
+            return res.redirect('back')
+        })
+    } else{
+        return res.redirect('back')
+    }
+   })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const postsController = require('../controllers/postController');
 
 router.post('/create', postsController.create);
-
+router.get('/destroy/:id', postsController.destroy);
 
 module.exports = router;
